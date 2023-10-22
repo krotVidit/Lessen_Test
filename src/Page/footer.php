403444ffe44846f79603c5ab8f23fc50ab5f7d30
KrotVidit <backendkid@mail.ru>
2023-10-23 00:15:31 +0300

    Тестим

---
+++ src/Page/footer.php
@@ -0,0 +1,5 @@
+<script src = "JavaSkript/Lessen2.js">
+
+</script>
+</body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once ("Page/header.php");
+
+
+
+
+
+require_once ("Page/footer.php");
+?>
+
+
+
